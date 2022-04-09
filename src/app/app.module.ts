a4c28b44d4cd77b0b2d9cbd274a6003d6bfad799
lukeferenc <luke.theduke.of.cornwall@gmail.com>
2022-04-10 01:27:58 +0200

    6.4 creating edit-user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { UserLoginFormComponent } from './user-login-form/user-login-form.compon
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
 
 const appRoutes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
@@ -33,7 +34,8 @@ const appRoutes: Routes = [
     UserLoginFormComponent,
     MovieCardComponent,
     WelcomePageComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    EditUserComponent
   ],
   imports: [
     BrowserModule,
