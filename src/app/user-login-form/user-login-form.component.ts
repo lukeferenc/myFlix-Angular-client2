9de4f7ab00c003023a6dbd0b1ef002814113359d
lukeferenc <luke.theduke.of.cornwall@gmail.com>
2022-04-09 21:11:41 +0200

    6.4 welcome page + fixing moviecard

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login-form',
@@ -15,7 +16,9 @@ export class UserLoginFormComponent implements OnInit {
   constructor(
       public fetchApiData: FetchApiDataService,
       public dialogRef: MatDialogRef<UserLoginFormComponent>,
-      public snackBar: MatSnackBar) { }
+      public snackBar: MatSnackBar,
+      public router: Router
+      ) { }
 
   ngOnInit(): void {
   }
@@ -28,6 +31,7 @@ export class UserLoginFormComponent implements OnInit {
       this.snackBar.open('You are now logged in', 'OK', {
           duration: 2000
       });
+      this.router.navigate(['movies']);
     }, (response) => {
       console.log(response)
       this.snackBar.open('Something went wrong! ', 'OK', {
