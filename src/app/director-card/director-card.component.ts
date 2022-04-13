1bf75bf87baaa1e251cc3265131f18192b2a3c95
lukeferenc <luke.theduke.of.cornwall@gmail.com>
2022-04-13 03:03:23 +0200

    Updating director-card

--- src/app/director-card/director-card.component.ts
+++ src/app/director-card/director-card.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Data } from '@angular/router';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
 
 @Component({
   selector: 'app-director-card',
   templateUrl: './director-card.component.html',
-  styleUrls: ['./director-card.component.scss']
+  styleUrls: ['./director-card.component.scss'],
 })
 export class DirectorCardComponent implements OnInit {
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    @Inject(MatDialog)
+    public data: {
+      name: string;
+      bio: string;
+      birthdate: Date;
+    }
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
