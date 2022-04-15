31962a16282e7146439b63f34780bec0c718889c
lukeferenc <luke.theduke.of.cornwall@gmail.com>
2022-04-15 05:40:09 +0200

    6.4 working on delete-user

--- src/app/delete-user/delete-user.component.ts
+++ src/app/delete-user/delete-user.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-delete-user',
   templateUrl: './delete-user.component.html',
-  styleUrls: ['./delete-user.component.scss']
+  styleUrls: ['./delete-user.component.scss'],
 })
-export class DeleteUserComponent implements OnInit {
+export class RemoveUserComponent implements OnInit {
+  user: any = JSON.parse(localStorage.getItem('user') || '');
 
-  constructor() { }
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public snackbar: MatSnackBar,
+    public router: Router
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  deleteUser(): void {
+    this.fetchApiData.removeUserProfile().subscribe(() => {
+      this.snackbar.open(`${this.user} has been deleted`, 'OK', {
+        duration: 4000,
+      });
+      localStorage.clear();
+    });
+    this.router.navigate(['welcome']);
   }
-
 }
